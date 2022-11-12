40b4bb02f27de4f7b0d6885ee484857dc680c625
trisnayudha <yudha@indonesiaminer.com>
2022-11-12 22:29:46 +0700

    fix bug qrcode

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -407,7 +407,7 @@ Your verification code (OTP) ' . $otp;
                 if (!empty($findUser)) {
                     $image = QrCode::format('png')
                         ->size(300)->errorCorrection('H')
-                        ->generate('ABDC');
+                        ->generate($codePayment);
                     $output_file = '/public/upload/qr-code/img-' . time() . '.png';
                     $output_db = '/storage/upload/qr-code/img-' . time() . '.png';
                     Storage::disk('local')->put($output_file, $image); //storage/app/public/img/qr-code/img-1557309130.png
