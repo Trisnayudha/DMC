8e5f6f1205c258ef983fe218f47b8fe387f8fc2c
trisnayudha <yudha@indonesiaminer.com>
2022-12-12 11:19:33 +0700

    update test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,46 +1,15 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <style>
-        body {
-            font-family: 'Nunito';
-        }
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
 
 <body>
-    <!-- if validation in the controller fails, show the errors -->
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <div>
-
-        <form action="{{ url('test/upload') }}" method="post" enctype="multipart/form-data">
-            <!-- Add CSRF Token -->
-            @csrf
-            <div class="form-group">
-                <input type="file" name="image" required>
-            </div>
-            <button type="submit">Submit</button>
-        </form>
-
-    </div>
+    <a href="discipleapp://">Open My App</a>
 </body>
 
 </html>
