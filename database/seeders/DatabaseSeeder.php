834715f382b660458298f9df774f71aa397f8e6e
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2023-01-27 15:53:10 +0700

    seeder sponsor

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // $this->call(PermissionTableSeeder::class);
         // $this->call(RoleSeeder::class);
         // $this->call(UserSeeder::class);
-        // $this->call(SponsorSeeder::class);
+        $this->call(SponsorSeeder::class);
         // $this->call(NewsSeeder::class);
-        $this->call(FaqSeeder::class);
+        // $this->call(FaqSeeder::class);
     }
 }
