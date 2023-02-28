a084f3a5f03986952943da50a589eb1b4ca1bcfa
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2023-02-28 11:56:32 +0700

    updat event special

--- app/Http/Controllers/SpecialEventController.php
+++ app/Http/Controllers/SpecialEventController.php
@@ -9,6 +9,16 @@ use Illuminate\Support\Str;
 
 class SpecialEventController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function free()
     {
         return view('special_event.free');
