68c8ac7b59eede5c9e484981221c895e2b07bd89
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2023-02-28 15:12:15 +0700

    update image

--- resources/views/email/special_event/waiting-approval.blade.php
+++ resources/views/email/special_event/waiting-approval.blade.php
@@ -105,7 +105,7 @@
 
     <div class="body">
         <div class="header-email">
-            <img src="https://api.djakarta-miningclub.com/image/logo-dmc-cci.png" alt="Image" class="img-logo">
+            <img src="{{ asset('image/logo-dmc-cci.png') }}" alt="Image" class="img-logo">
         </div>
         <div class="body-email">
             <p>Dear {{ $users_name }},</p>
