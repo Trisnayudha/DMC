97079afe23c6d4d9044f39464e7568a642c733fa
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2023-03-08 14:37:23 +0700

    update fix bug

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -990,7 +990,7 @@ class EventController extends Controller
                 'image' => $db
             ];
             // dd("sukses");
-            ini_set('max_execution_time', 120);
+            // ini_set('max_execution_time', 120);
 
             $pdf = Pdf::loadView('email.ticket', $data);
             Mail::send('email.approval-event', $data, function ($message) use ($email, $pdf, $codePayment, $findEvent) {
