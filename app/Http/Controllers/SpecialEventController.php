9ad699a35888ff94d0661f229adedb0fca6e7c35
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2023-05-25 11:02:52 +0700

    update hp dmc

--- app/Http/Controllers/SpecialEventController.php
+++ app/Http/Controllers/SpecialEventController.php
@@ -62,7 +62,7 @@ class SpecialEventController extends Controller
             $send->sendEmail();
 
             $wa = new WhatsappApi();
-            $wa->phone = '081387739341';
+            $wa->phone = '08111798791';
             $wa->message = '
 Hai Mas Damun,
 
