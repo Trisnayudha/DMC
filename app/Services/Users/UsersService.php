97a853d0b3825665919a65c26042259499bf6cb1
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2023-08-01 13:41:00 +0700

    change category

--- app/Services/Users/UsersService.php
+++ app/Services/Users/UsersService.php
@@ -54,23 +54,35 @@ class UsersService extends User
     private static function mapCategory($category, $otherCategory)
     {
         $categoryMap = [
-            'Coal Mining' => 'Coal Mining/Minerals producer/Contractor',
-            'Minerals Producer' => 'Coal Mining/Minerals producer/Contractor',
-            'Contractor' => 'Coal Mining/Minerals producer/Contractor',
-            'Supplier/Distributor/Manufacturer' => 'Supplier/Distributor/Manufaturer/Technology',
-            'Consultant' => 'Consultan/Association/Organization/Goverment',
-            'Logistics and Shipping' => 'Service/Logistic/Shipping',
+            'Coal Mining' => 'Category 1',
+            'Minerals Producer' => 'Category 1',
+            'Contractor' => 'Category 1',
+            'Supplier/Distributor/Manufacturer' => 'Category 2',
+            'Consultant' => 'Category 3',
             'Association / Organization / Government' => 'Consultan/Association/Organization/Goverment',
-            'Financial Services' => 'Service/Logistic/Shipping',
-            'Investors' => 'Service/Logistic/Shipping',
-            'Media' => 'Service/Logistic/Shipping',
-            'other' => 'Other',
+            'Logistics and Shipping' => 'Category 4',
+            'Financial Services' => 'Category 4',
+            'Investors' => 'Category 4',
+            'Media' => 'Category 4',
+            'other' => 'Category 5',
         ];
-
+        // $categoryMap = [
+        //     'Coal Mining' => 'Coal Mining/Minerals producer/Contractor',
+        //     'Minerals Producer' => 'Coal Mining/Minerals producer/Contractor',
+        //     'Contractor' => 'Coal Mining/Minerals producer/Contractor',
+        //     'Supplier/Distributor/Manufacturer' => 'Supplier/Distributor/Manufaturer/Technology',
+        //     'Consultant' => 'Consultan/Association/Organization/Goverment',
+        //     'Association / Organization / Government' => 'Consultan/Association/Organization/Goverment',
+        //     'Logistics and Shipping' => 'Service/Logistic/Shipping',
+        //     'Financial Services' => 'Service/Logistic/Shipping',
+        //     'Investors' => 'Service/Logistic/Shipping',
+        //     'Media' => 'Service/Logistic/Shipping',
+        //     'other' => 'Other',
+        // ];
         if (isset($categoryMap[$category])) {
             return $categoryMap[$category];
         } elseif ($category === 'Other' || empty($otherCategory)) {
-            return 'Other';
+            return 'Category 5';
         }
     }
 
