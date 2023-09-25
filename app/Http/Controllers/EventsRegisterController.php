98f200900fb942f612aad3c6ee9a064be82de27b
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2023-09-25 09:19:41 +0700

    update checkphone

--- app/Http/Controllers/EventsRegisterController.php
+++ app/Http/Controllers/EventsRegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Events\Events;
+use App\Models\Profiles\ProfileModel;
 use Illuminate\Http\Request;
 
 class EventsRegisterController extends Controller
@@ -17,5 +18,17 @@ class EventsRegisterController extends Controller
     {
         $findEvent = Events::where('slug', $slug)->first();
         return view('register_event.multiple', $findEvent);
+    }
+
+    public function checkPhone($phone)
+    {
+        $check = ProfileModel::where('phone', $phone)->first();
+
+        $status = $check != null ? 1 : 0; // Menggunakan operator ternary untuk mengatur status
+
+        return response()->json([
+            'status' => $status,
+            'message' => $status ? 'Nomor telepon sudah digunakan' : 'Nomor telepon tersedia',
+        ]);
     }
 }
