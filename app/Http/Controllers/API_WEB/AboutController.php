b53c4abe671b33bfbf4e9dcbd3d8277ffec0c3b3
Trisnayudha <trisnayuhabachtiar@gmail.com>
2023-12-09 16:28:38 +0700

    update : about kekurangan 1 image

--- app/Http/Controllers/API_WEB/AboutController.php
+++ app/Http/Controllers/API_WEB/AboutController.php
@@ -14,7 +14,8 @@ class AboutController extends Controller
             'https://djakarta-miningclub.com/wp-content/uploads/2023/09/56-DMC_03Acara_460-1.jpg',
             'https://djakarta-miningclub.com/wp-content/uploads/2023/09/DSC4058-scaled.jpg',
             'https://djakarta-miningclub.com/wp-content/uploads/2023/07/DSC03504-1.jpg',
-            'https://djakarta-miningclub.com/wp-content/uploads/2023/07/AYM08873.jpg'
+            'https://djakarta-miningclub.com/wp-content/uploads/2023/07/AYM08873.jpg',
+            'https://djakarta-miningclub.com/wp-content/uploads/2022/12/DSC00152.jpg'
         ];
         $response['status'] = 200;
         $response['message'] = 'Success';
