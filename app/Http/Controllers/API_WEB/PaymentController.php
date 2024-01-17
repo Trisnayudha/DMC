54e41ef1b97ac975b3b3baa5c04e507d90d992fc
Trisnayudha <trisnayuhabachtiar@gmail.com>
2024-01-17 13:21:13 +0700

    update history payment

--- app/Http/Controllers/API_WEB/PaymentController.php
+++ app/Http/Controllers/API_WEB/PaymentController.php
@@ -13,6 +13,8 @@ class PaymentController extends Controller
         $limit = $request->limit ?? 5;
         $id =  auth('sanctum')->user()->id;
         $findPayment = Payment::where('member_id', '=', $id)
+            ->join('events_tickets', 'events_tickets.id', 'payment.tickets_id')
+            ->select('payment.*', 'events_tickets.price_rupiah')
             ->whereIn('package', ['nonmember', 'onsite', 'member', 'Premium'])
             ->orderby('id', 'desc')->paginate($limit);
         $response['status'] = 200;
