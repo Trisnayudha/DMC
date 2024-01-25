2826cf7bbfcd4b899d6c60622e7e80760f4d21a8
Trisnayudha <trisnayuhabachtiar@gmail.com>
2024-01-25 10:33:54 +0700

    fix bug

--- app/Http/Controllers/API/HomeController.php
+++ app/Http/Controllers/API/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $event = Events::select('id', 'name as title', 'description as desc', 'slug', 'image', 'image_banner')->orderBy('id', 'desc')->limit(3)->get();
+        $event = Events::where('stauts', 'publish')->select('id', 'name as title', 'description as desc', 'slug', 'image', 'image_banner')->orderBy('id', 'desc')->limit(3)->get();
         foreach ($event as $key) {
             $key->type = 'events';
             $key->desc = (strlen($key->desc) > 50 ? substr($key->desc, 0,  50) . '...' : $key->desc);
