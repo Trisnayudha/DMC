b4c5036b3ac7813a00734d667322c662af5c844b
Trisnayudha <trisnayuhabachtiar@gmail.com>
2024-04-29 11:57:16 +0700

    fix bug stardate n endtime index list

--- app/Http/Controllers/API_WEB/EventsController.php
+++ app/Http/Controllers/API_WEB/EventsController.php
@@ -39,8 +39,8 @@ class EventsController extends Controller
             $key->start_date = (!empty($key->start_date) ? date('d M Y', strtotime($key->start_date)) : '');
             $key->end_date = (!empty($key->end_date) ? date('d M Y', strtotime($key->end_date)) : '');
             // Ambil jam dan menit dari start_time dan end_time
-            $key->start_time = (!empty($key->start_time) ? date('H:i', strtotime($key->start_time)) : '');
-            $key->end_time = (!empty($key->end_time) ? date('H:i', strtotime($key->end_time)) : '');
+            $key->start_time = (!empty($key->start_time) ? date('h:i', strtotime($key->start_time)) : '');
+            $key->end_time = (!empty($key->end_time) ? date('h:i A', strtotime($key->end_time)) : '');
         }
         $response['status'] = 200;
         $response['message'] = 'Success';
