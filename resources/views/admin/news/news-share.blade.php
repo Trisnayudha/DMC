41789ff583dd8dcc409070370b68e8091093259c
Trisnayudha <trisnayuhabachtiar@gmail.com>
2024-06-27 15:18:41 +0700

    update

--- resources/views/admin/news/news-share.blade.php
+++ resources/views/admin/news/news-share.blade.php
@@ -4,11 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="{{ $news->desc }}">
+    <meta name="keyword" content="{{ $news->title }}">
+    <meta name="context" content="https://djakarta-miningclub.com/news/{{ $news->slug }}">
+    <meta name="path" content="{{ base64_encode(url('/')) }}">
+    <meta name="asset" content="{{ base64_encode(asset('/')) }}">
+
+    <meta property="og:type" content="article" />
+    <meta property="og:locale" content="en_GB" />
+    <meta property="og:locale:alternate" content="fr_FR" />
+    <meta property="og:locale:alternate" content="es_ES" />
     <meta property="og:title" content="{{ $news->title }}">
     <meta property="og:description" content="{{ $news->desc }}">
     <meta property="og:image" content="{{ url($news->image) }}">
     <meta property="og:url" content="https://djakarta-miningclub.com/news/{{ $news->slug }}">
     <meta property="og:type" content="article">
+    <meta http-equiv='cache-control' content='no-cache'>
+    <meta http-equiv='expires' content='0'>
+    <meta http-equiv='pragma' content='no-cache'>
     <title>DMC News: {{ $news->title }}</title>
 </head>
 
