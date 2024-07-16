0ad8b792799910d169b5b8b9be015e1e68858f6a
Trisnayudha <trisnayuhabachtiar@gmail.com>
2024-07-16 10:44:58 +0700

    update

--- resources/views/admin/events/event-share.blade.php
+++ resources/views/admin/events/event-share.blade.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="description" content="{{ $event->description }}">
-    <meta name="keyword" content="{{ $event->title }}">
+    <meta name="keyword" content="{{ $event->name }}">
     <meta name="context" content="https://djakarta-miningclub.com/event/{{ $event->slug }}">
-    <meta name="description" content="DMC event: {{ $event->title }}" />
-    <meta property="og:title" content="{{ $event->title }}">
+    <meta name="description" content="DMC event: {{ $event->name }}" />
+    <meta property="og:title" content="{{ $event->name }}">
     <meta property="og:image" content="https://membership.djakarta-miningclub.com/{{ $event->image }}">
     <meta property="og:type" content="article" />
     <meta property="og:locale" content="en_GB" />
@@ -20,7 +20,7 @@
     <meta http-equiv='cache-control' content='no-cache'>
     <meta http-equiv='expires' content='0'>
     <meta http-equiv='pragma' content='no-cache'>
-    <title>DMC Event: {{ $event->title }}</title>
+    <title>DMC Event: {{ $event->name }}</title>
     <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/checkout/">
 </head>
 
