e90b24b813e1c082aef4390372364f870a635e56
Trisnayudha <trisnayuhabachtiar@gmail.com>
2024-08-20 15:56:16 +0700

    update

--- app/Http/Controllers/Admin/EventsDetailParticipantController.php
+++ app/Http/Controllers/Admin/EventsDetailParticipantController.php
@@ -368,7 +368,9 @@ class EventsDetailParticipantController extends Controller
         //         ';
         $send->message = 'We haven’t  seen you yet!
 Just a quick reminder that our event ' . $event->name . ' at The Dharmawangsa Hotel now has already started, we`re curious to know what`s keeping you from joining us.
+
 There`s still so much happening here that you won`t want to miss out on—insightful sessions, panel discussion, great networking opportunities, and more.
+
 If there`s anything holding you back, feel free to reach out. We’d love to have you here!
 ';
         // $send->document = asset($db);
