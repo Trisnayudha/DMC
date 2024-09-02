d77aece864f15f629a0918ff5ecef4076b3927a5
Trisnayudha <trisnayuhabachtiar@gmail.com>
2024-09-02 10:36:27 +0800

    update

--- app/Http/Controllers/API_WEB/HomeController.php
+++ app/Http/Controllers/API_WEB/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends Controller
 
     public function getCarousel()
     {
-        $signature = null;
+        $signature = 'image/jexpo.png';
         $events = Events::select('id', 'name', 'description', 'slug', 'start_date')
             ->orderBy('id', 'desc')
             ->get();
