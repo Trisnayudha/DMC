19fd70145b7b28958c52ce205ad62f90c64478c2
Trisnayudha <trisnayuhabachtiar@gmail.com>
2024-09-05 14:56:23 +0800

    update

--- app/Http/Controllers/API_WEB/HomeController.php
+++ app/Http/Controllers/API_WEB/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends Controller
 
     public function getCarousel()
     {
-        $signature = '/storage/artwork/libur.png';
+        $signature = '/images/libur3.png';
         $events = Events::select('id', 'name', 'description', 'slug', 'start_date')
             ->orderBy('id', 'desc')
             ->get();
