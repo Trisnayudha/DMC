b4fec4322000f450adacc769b23ae77bb6d36d18
Yudha <ingganasution@Linggas-MacBook-Air.local>
2025-03-19 13:04:07 +0700

    update

--- resources/views/email/approval-event.blade.php
+++ resources/views/email/approval-event.blade.php
@@ -82,7 +82,7 @@
         }
 
         .img-logo {
-            /* height: 70px; */
+            height: 150px;
             display: block;
             margin-left: auto;
             margin-right: auto;
