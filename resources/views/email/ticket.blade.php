d6197183a9b8fd9777ff9e43e3e0cc41886bb066
Yudha <ingganasution@Linggas-MacBook-Air.local>
2025-03-19 11:32:09 +0700

    update

--- resources/views/email/ticket.blade.php
+++ resources/views/email/ticket.blade.php
@@ -122,7 +122,7 @@
 
     <div class="body">
         <div class="header-email">
-            <img src="https://api.djakarta-miningclub.com/image/logo-dmc-mcclosky.png" alt="Image" class="img-logo">
+            <img src="https://membership.djakarta-miningclub.com/image/logo-anniv.png" alt="Image" class="img-logo">
         </div>
         <div class="body-email">
             <p>Dear {{ $users_name }},</p>
