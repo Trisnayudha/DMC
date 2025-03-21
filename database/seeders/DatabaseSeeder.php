5cfe9d8954bec54de591d639c8721148be56f934
Yudha <ingganasution@Linggas-MacBook-Air.local>
2025-03-21 13:48:37 +0700

    update db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,12 +15,12 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(PermissionTableSeeder::class);
         // $this->call(RoleSeeder::class);
-        // $this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
         // $this->call(SponsorSeeder::class);
         // $this->call(NewsSeeder::class);
         // $this->call(FaqSeeder::class);
-        $this->call(BenefitSeeder::class);
-        $this->call(PackageBenefitSeeder::class);
-        $this->call(SponsorBenefitUsageSeeder::class);
+        // $this->call(BenefitSeeder::class);
+        // $this->call(PackageBenefitSeeder::class);
+        // $this->call(SponsorBenefitUsageSeeder::class);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,32 +15,32 @@ class UserSeeder extends Seeder
     public function run()
     {
         $admin = User::create([
-            'name' => 'Erina',
-            'email' => 'erina@indonesiaminer.com',
-            'password' => bcrypt('12345')
+            'name' => 'M. Rifkie',
+            'email' => 'mrifkie@indonesiaminer.com',
+            'password' => bcrypt('mrifkie123')
         ]);
         $admin->assignRole('admin');
 
-        $user = User::create([
-            'name' => 'Callula',
-            'email' => 'calulla@indonesiaminer.com',
-            'password' => bcrypt('12345')
-        ]);
-        $user->assignRole('admin');
+        // $user = User::create([
+        //     'name' => 'Callula',
+        //     'email' => 'calulla@indonesiaminer.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+        // $user->assignRole('admin');
 
-        $damun = User::create([
-            'name' => 'Damun',
-            'email' => 'damun@indonesiaminer.com',
-            'password' => bcrypt('12345')
-        ]);
-        $damun->assignRole('admin');
+        // $damun = User::create([
+        //     'name' => 'Damun',
+        //     'email' => 'damun@indonesiaminer.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+        // $damun->assignRole('admin');
 
-        $cahyani = User::create([
-            'name' => 'Cahyani',
-            'email' => 'cahyani@djakarta-miningclub.com',
-            'password' => bcrypt('12345')
-        ]);
-        $cahyani->assignRole('admin');
+        // $cahyani = User::create([
+        //     'name' => 'Cahyani',
+        //     'email' => 'cahyani@djakarta-miningclub.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+        // $cahyani->assignRole('admin');
 
         // $dumy = User::factory(25)->create();
     }
