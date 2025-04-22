8d4bc96560ff8db7eb8b596a23f5fa54a00d1fa9
Yudha <ingganasution@Linggas-MacBook-Air.local>
2025-04-22 12:44:26 +0700

    update

--- app/Http/Controllers/Admin/PaymentController.php
+++ app/Http/Controllers/Admin/PaymentController.php
@@ -93,7 +93,6 @@ class PaymentController extends Controller
                     $subtotal -= $check->discount;
                 }
                 $total = $subtotal;
-                dd($total);
                 $params = [
                     'external_id' => $check->code_payment,
                     'payer_email' => $findUsers->email,
