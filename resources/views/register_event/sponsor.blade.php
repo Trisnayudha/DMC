594a4a8ea8e402c949051611e3963f0fa00a0258
Yudha <yudha@indonesiaminer.com>
2025-06-19 14:02:42 +0700

    update dmc 24 july

--- resources/views/register_event/sponsor.blade.php
+++ resources/views/register_event/sponsor.blade.php
@@ -95,7 +95,7 @@
         <main>
             <div class="py-2 text-center">
                 <img style="border-radius: 15px; margin-bottom: 19px; height: 120px; "
-                    src="{{ asset('image/dmc.png') }}" class="img-fluid" alt="">
+                    src="{{ asset('image/logo-dmc-pwc.png') }}" class="img-fluid" alt="">
                 <h2 style="text-transform: uppercase">REGISTER EVENT
                 </h2>
                 {{-- <p class="lead"> The 53rd Networking Event - Djakarta Mining Club and Coal Club Indonesia x McCloskey
