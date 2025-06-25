f56fcf56941ff64f557c71e2d659b748cc9c7fef
Yudha <yudha@indonesiaminer.com>
2025-06-25 09:59:12 +0700

    updat

--- resources/views/email/approval-event.blade.php
+++ resources/views/email/approval-event.blade.php
@@ -175,7 +175,7 @@
             </table>
 
             <p>View the latest of the event program by clicking here
-                (https://www.djakarta-miningclub.com/events/the-12th-anniversary-of-djakarta-mining-club)
+                (https://www.djakarta-miningclub.com/events/the-67th-djakarta-mining-club-networking-event)
             </p>
 
             <br>
