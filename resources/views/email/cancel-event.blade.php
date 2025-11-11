1d1dcd6295bc1f41fa4d54a21e52a07719e75bf6
Yudha <yudha@indonesiaminer.com>
2025-11-11 10:59:45 +0700

    fix bug

--- resources/views/email/cancel-event.blade.php
+++ resources/views/email/cancel-event.blade.php
@@ -65,7 +65,7 @@
                 Your seat will be offered to participants on the waiting list.</p>
 
             <p>If you believe this is an error or would like further assistance, please contact us at
-                secretariat@djakarta-miningclub.com or +62 811 1937 300.</b>.
+                secretariat@djakarta-miningclub.com or +62 811 1937 300.</b>
             </p>
 
             <p>We apologize for any inconvenience caused. We look forward to welcoming you at our future events.
