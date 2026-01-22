52a0e07c5bacbe5f4c6a6a976b08fdf9a30b623e
Yudha <yudha@indonesiaminer.com>
2026-01-22 16:01:47 +0700

    fix QR using bacon v2 correct backend setup

--- app/Support/QrCode.php
+++ app/Support/QrCode.php
@@ -2,7 +2,7 @@
 
 namespace App\Support;
 
-use BaconQrCode\Renderer\Image\Png;
+use BaconQrCode\Renderer\ImageRenderer;
 use BaconQrCode\Renderer\RendererStyle\RendererStyle;
 use BaconQrCode\Writer;
 
@@ -23,14 +23,14 @@ class QrCode
 
     public function errorCorrection(string $level)
     {
-        // bacon v2 default already OK
         return $this;
     }
 
     public function generate(string $text): string
     {
-        $renderer = new Png(
-            new RendererStyle($this->size)
+        $renderer = new ImageRenderer(
+            new RendererStyle($this->size),
+            new \BaconQrCode\Renderer\Image\GdImageBackEnd()
         );
 
         $writer = new Writer($renderer);
