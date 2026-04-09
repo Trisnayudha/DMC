26e5fc2e316a800641145ff7cedc20651aecb0b3
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2026-04-09 08:42:44 +0700

    updat

--- resources/views/admin/events/event-detail-participant.blade.php
+++ resources/views/admin/events/event-detail-participant.blade.php
@@ -88,6 +88,10 @@
                                 <i class="fas fa-star mr-1"></i>Sponsor
                                 <span class="pill-count">{{ $sponsorCount }}</span>
                             </button>
+                            <button class="pill pill-sponsor" data-filter="free">
+                                <i class="fas fa-star mr-1"></i>Invitation/Free
+                                <span class="pill-count">{{ $freeCount }}</span>
+                            </button>
                         </div>
                     </div>
 
