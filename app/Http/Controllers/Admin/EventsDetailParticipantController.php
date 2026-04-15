69f3e1ee94532c7ad4717293449e9cc42a140ed2
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2026-04-15 08:08:59 +0700

    update

--- app/Http/Controllers/Admin/EventsDetailParticipantController.php
+++ app/Http/Controllers/Admin/EventsDetailParticipantController.php
@@ -434,7 +434,7 @@ The Djakarta Mining Club Team';
 
 Hi ' . $data['users_name'] . ',
 
-This is a confirmation that you are registered to attend our event on TOMORROW, 15 April 2026 at ' . $event->location . ', starting at ' . date('h.i a', strtotime($event->start_time)) . ' - ' . date('h.i a', strtotime($event->end_time)) . ' (WIB) and followed by Networking Dinner and Drinks.
+This is a confirmation that you are registered to attend our event on TODAY, 15 April 2026 at ' . $event->location . ', starting at ' . date('h.i a', strtotime($event->start_time)) . ' - ' . date('h.i a', strtotime($event->end_time)) . ' (WIB) and followed by Networking Dinner and Drinks.
 
 Please confirm your attendance by replying "YES" to this message. If you are unable to attend, kindly respond with "NO" so that we may offer your spot to someone on the waitlist.
 
