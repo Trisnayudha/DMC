9c3000ab27b14dc5526052e3c210874ded441764
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2026-06-18 10:11:18 +0700

    fix

--- resources/views/admin/users/partials/_scripts.blade.php
+++ resources/views/admin/users/partials/_scripts.blade.php
@@ -1,4 +1,6 @@
 @push('bottom')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script>
     // CSRF untuk semua AJAX
     $.ajaxSetup({
