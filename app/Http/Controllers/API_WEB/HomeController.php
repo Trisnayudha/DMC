c121bd6f193ada1cd0c03acca109d3baf52725dd
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2026-09-17 15:09:58 +0700

    update test

--- app/Http/Controllers/API_WEB/HomeController.php
+++ app/Http/Controllers/API_WEB/HomeController.php
@@ -22,7 +22,7 @@ class HomeController extends Controller
         // 2 signature terpisah (urutannya sesuai array)
         $signatures = [
             //     // '/image/libur5.png',
-            // '/image/libur9-1.png',
+            '/image/banner_ctaasia_1.jpeg',
         ];
 
         $events = Events::select('id', 'name', 'description', 'slug', 'start_date')
